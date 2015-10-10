549768381b63e7a10a7fcba44632476dc38da414
Justin Cano <jcano001@ucr.edu>
2015-10-10 01:53:43 -0700

    added some dependency packages

--- scripts/install/installDependencies.sh
+++ scripts/install/installDependencies.sh
@@ -7,3 +7,9 @@ apt-get install nfs-kernel-server portmap nfs-common
 
 # Install exFAT tools
 apt-get install exfat-fuse exfat-utils
+
+# Install Python tools
+apt-get install python-pip flask
+
+# Install Beets
+pip install beets
